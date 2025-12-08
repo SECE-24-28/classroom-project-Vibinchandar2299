a9f981bb56b53747f7efa4493e769f4625e7c43a
Vibinchandar <vibinmarutharajavp@gmail.com>
2025-12-08 14:11:20 +0530

    included header and contents for the react project

---
+++ first_project/src/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Header = () => {
+    let des={background:"black",color:"white"}
+	return (
+		
+        <div>
+<h1 style={des}>Vibin</h1>
+<p>Hello From Vibin</p>
+
+        </div>
+	)
+}
+
+export default Header
