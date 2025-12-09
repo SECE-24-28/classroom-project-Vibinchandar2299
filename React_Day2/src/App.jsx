640a30a931d54fab0c40b15f626a378f6e27eeb1
Vibinchandar <vibinmarutharajavp@gmail.com>
2025-12-09 14:36:56 +0530

    Updated Day 2 React Project

--- React_Day2/src/App.jsx
+++ React_Day2/src/App.jsx
@@ -2,44 +2,59 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import AddStudent from './AddStudent'
+import SearchStudent from './SearchStudent'
+import Body from './Body'
 
-function App() {
-      const [list,setList] =useState([
-        {id:1,sname:"Vinoth",fee:true},
-        {id:2,sname:"Shiva",fee:false},
-        {id:3,sname:"Harish",fee:true}
-      ]);
+function App() 
+{
+    const [stuList,setStuList]=useState([
+      {id:1,sname:"Jack",fee:true},
+      {id:2,sname:"Smith",fee:false},
+      {id:3,sname:"Victor",fee:true}
+    ])
 
-      const handleDelete=(id)=>
-      {
-      const newList=list.filter((ls)=>ls.id!=id)
-      setList(newList)
-      }
-      const handleCheck=(id)=>
-      {
-         const newList=list.map((ls)=>(ls.id===id)?({...ls,fee:!ls.fee}):(ls))
-         setList(newList)
-      }
+    const handleDelete=(id)=>
+    {
+      const newList=stuList.filter((stu)=>stu.id!==id)
+      setStuList(newList)
+    }
+    const handleChange=(id)=>
+    {
+      const newList=stuList.map((stu)=>(stu.id===id)?({...stu,fee:!stu.fee}):(stu))
+      setStuList(newList)
+    }
+   const addItem=(item)=>
+   {
+    const sz=stuList.length-1;
+     const nid=(stuList.length)?(stuList[sz].id+1):(1)
+     const newObj={id:nid,sname:item,fee:false}
+     const newList=[...stuList,newObj]
+     setStuList(newList)
+   }
+   const handleSubmit=(e)=>
+   {
+    e.preventDefault()
+    addItem(newstu)
+    setNewStu('')
+   }
+    const [newstu,setNewStu]=useState('')
+    const [search,setSearch]=useState('')
   return (
     <>
-         <h1>Students List</h1>
-         <hr />
-        <ul>
-        {
-           list.map((ls)=>
-            <li key={ls.id}>
-                  <input type="checkbox" checked={ls.fee} 
-                                         onChange={()=>handleCheck(ls.id)} />
-                  <label>{ls.sname}</label>
-                  <button onClick={()=>handleDelete(ls.id)}>Delete</button>
-            </li>
-          )
-        }
-        </ul>
-           
-           <br /><br />
-
-           <p>Student count : {list.length}</p>
+       <h1>Students List</h1>
+       <AddStudent  newstu={newstu}
+                    setNewStu={setNewStu}
+                    handleSubmit={handleSubmit}
+       />
+       <SearchStudent search={search}
+                      setSearch={setSearch} 
+       />
+       
+       <Body stuList={stuList.filter((list)=>(list.sname).includes(search))}
+             handleChange={handleChange}
+             handleDelete={handleDelete} 
+       />
     </>
   )
 }
