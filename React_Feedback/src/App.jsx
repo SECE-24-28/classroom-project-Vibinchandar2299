94a928551d3461b33627a48c1104d82a6b82dd37
Vibinchandar <vibinmarutharajavp@gmail.com>
2025-12-12 10:04:51 +0530

    added home and addpost page with react routing for the feedback

--- React_Feedback/src/App.jsx
+++ React_Feedback/src/App.jsx
@@ -4,17 +4,26 @@ import Home from './Home.jsx'
 import Search from './Search.jsx'
 import AddPost from './AddPost.jsx'
 import { DataProvider } from './DataContext.jsx'
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom'
 
 function App() {  
   return (
-    <>
-       <DataProvider>
-          <Search/><hr />
-          <AddPost/>
-         <Home/>
-       </DataProvider>
-       
-    </>
+    <DataProvider>
+      <Router>
+        <Search />
+        <hr />
+
+        <ol>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/newpost">NewPost</Link></li>
+        </ol>
+
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/newpost" element={<AddPost />} />
+        </Routes>
+      </Router>
+    </DataProvider>
   )
 }
 
